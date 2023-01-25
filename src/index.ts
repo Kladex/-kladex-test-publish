e20f6649fe7b1b42dbadbe4efa365b2f2c1b5999
Suwatcharin Issariyakasem <suwatcharinissariyakasem@adsl-172-10-0-168.dsl.sndg02.sbcglobal.net>
2023-01-25 16:38:27 +0700

    fix: all tsconfig fixed

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-import { helloWorld, goodBye } from "./spec/function";
+export function helloWorld(): string {
+  const message = "Hello World from my example modern npm package!";
+  return message;
+}
 
-console.log(helloWorld());
+export function goodBye(): string {
+  const message = "Goodbye from my example modern npm package!";
+  return message;
+}
+
 console.log(goodBye());
+console.log(helloWorld());
+
+export default { goodBye, helloWorld };
